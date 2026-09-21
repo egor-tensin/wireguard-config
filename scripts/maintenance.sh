23dfc268a7381697bf1abdab41fd3e5a2dd2eb45
Egor Tensin <egor@tensin.name>
2026-09-21 23:17:54 +0500

    remove annoying trailing period from repo URLs

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2026 Egor Tensin <egor@tensin.name>
 # This file is part of the "WireGuard configuration" project.
-# For details, see https://github.com/egor-tensin/wireguard-config.
+# For details, see https://github.com/egor-tensin/wireguard-config
 # Distributed under the MIT License.
 
 set -o errexit -o nounset -o pipefail
